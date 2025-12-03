903331c5c1454c37b11aee8bb60f1fbb262dcdf4
Kirigami <131909245+s0zk@users.noreply.github.com>
2025-12-03 11:26:19 -0600

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
       '@typescript-eslint/no-unused-vars': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      "react/no-unescaped-entities": "warn"
     },
   }),
 ]
